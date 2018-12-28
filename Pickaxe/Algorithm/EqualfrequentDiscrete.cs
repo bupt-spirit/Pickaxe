a71426f67fb5e4d9e06cd711c03dd3b218136c70
DianaRenyao <857644432@qq.com>
2018-12-28 17:15:46 +0800

    等频分箱

---
+++ Pickaxe/Algorithm/EqualfrequentDiscrete.cs
@@ -0,0 +1,14 @@
+﻿using Pickaxe.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pickaxe.Algorithm
+{
+    class EqualfrequentDiscrete
+    {
+        public static void run(RelationAttribute attribute, int frequentNumber) { }
+    }
+}
