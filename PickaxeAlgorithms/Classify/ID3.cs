d9bfb36c3b655193b527dfcdb1fe28ec48c44c4b
DianaRenyao <857644432@qq.com>
2019-01-03 22:54:52 +0800

    pearson

---
+++ PickaxeAlgorithms/Classify/ID3.cs
@@ -0,0 +1,36 @@
+﻿using Pickaxe.AlgorithmFramework;
+using Pickaxe.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickaxeAlgorithms.Classify
+{
+    class ID3 : AlgorithmBase
+    {
+        public override AlgorithmType Type => AlgorithmType.Classify;
+
+        public override string Name => "ID3";
+
+        public override string Description => "ID3 is a classic classify algorithm based on decision-making tree" +
+            "It makes decision by caculationg information degree and information gain, chooses max one for further decision";
+
+        public ID3()
+        {
+            Options = new ObservableCollection<Option>
+            {
+                new Option("Attributes", "Attributes take part in ID3 classfication", typeof(IEnumerable<RelationAttribute>), null),
+                new Option("Label","Attribute that mark different class",typeof(RelationAttribute),null),
+            };
+        }
+
+        public override void Run()
+        {
+            var attributes = (IEnumerable<RelationAttribute>)Options[0].Value;
+            var label = (RelationAttribute)Options[1].Value;
+        }
+    }
+}
