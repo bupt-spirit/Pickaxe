1b7dd2fe709bc4578addff4c744982174bc3bb8b
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-01-04 10:09:20 +0800

    More powerful CSV loading

--- PickaxeCore/Model/RelationFormatter.cs
+++ PickaxeCore/Model/RelationFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -49,19 +50,58 @@ namespace Pickaxe.Model
                     relation.Add(new RelationAttribute(name, new AttributeType.Numeric(), new ObservableCollection<Value>()));
                 var tuplesView = relation.TuplesView;
                 var tupleIndex = 0;
+                var dictionaries = new HashSet<string>[relation.Count];
+
+                var isNominal = new bool[relation.Count];
+                for (int i = 0; i < relation.Count; ++i)
+                {
+                    dictionaries[i] = new HashSet<string>();
+                    isNominal[i] = false;
+                }
                 while (!parser.EndOfData)
                 {
                     tuplesView.Add(TupleView.Detached);
                     string[] fields = parser.ReadFields();
                     for (int i = 0; i < relation.Count; ++i)
                     {
-                        if (float.TryParse(fields[i], out var inFloat))
+                        var field = fields[i].Trim();
+                        if (isNominal[i])
                         {
-                            tuplesView[tupleIndex][i] = inFloat;
+                            if (dictionaries[i].Contains(field))
+                            {
+                                tuplesView[tupleIndex][i] = ((AttributeType.Nominal)relation[i].Type).NominalLabels.IndexOf(field);
+                            }
+                            else
+                            {
+                                var labels = ((AttributeType.Nominal)relation[i].Type).NominalLabels;
+                                var index = labels.Count;
+                                labels.Add(field);
+                                dictionaries[i].Add(field);
+                                tuplesView[tupleIndex][i] = Value.ToValue(index);
+                            }
                         }
                         else
                         {
-                            tuplesView[tupleIndex][i] = Value.MISSING;
+                            if (field == String.Empty)
+                            {
+                                tuplesView[tupleIndex][i] = Value.MISSING;
+                            }
+                            if (float.TryParse(fields[i], out var inFloat))
+                            {
+                                tuplesView[tupleIndex][i] = inFloat;
+                            }
+                            else
+                            {
+                                foreach (var v in relation[i].Data)
+                                {
+                                    if (!v.IsMissing()) {
+                                        throw new FormatException("Can not determine attribute type from csv");
+                                    }
+                                }
+                                relation[i].Type = new AttributeType.Nominal();
+                                isNominal[i] = true;
+                                --i; // reprocess this field
+                            }
                         }
                     }
                     ++tupleIndex;
